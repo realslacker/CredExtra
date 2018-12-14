a75871f4c5e213d12df519d53afcfbdbb873a289
Shannon Graybrook <552756+realslacker@users.noreply.github.com>
2018-12-14 14:55:29 -0600

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -12,25 +12,32 @@ $BuildDirectory = New-Item -Path "$ScriptPath\build\$BuildNumber\$ModuleName" -I
 Copy-Item -Path $ScriptPath\CredExtra.psd1 -Destination $BuildDirectory
 Copy-Item -Path $ScriptPath\lang -Destination $BuildDirectory -Recurse
 
+# copy all lib sub-directories to module
+Get-ChildItem -Path $ScriptPath\lib -Directory |
+    Copy-Item -Destination { "$BuildDirectory\lib\$($_.Name)" } -Recurse
+
 # create module file
 $ModuleFile = New-Item -Path "$BuildDirectory\$ModuleName.psm1" -ItemType File
 
 # array for exported functions
 $ExportModuleMembers = @()
 
-# add localization
-Add-Content -Path $ModuleFile -Value '#region LANG '.PadRight(80,'=')
-Add-Content -Path $ModuleFile -Value 'Import-LocalizedData -BindingVariable Messages -FileName Messages -BaseDirectory "$(Split-Path -Path (Get-Variable MyInvocation -Scope Script).Value.Mycommand.Definition -Parent)\lang"'
-Add-Content -Path $ModuleFile -Value '#endregion LANG '.PadRight(80,'=')
+# add common settings
+Add-Content -Path $ModuleFile -Value @'
+
+# Current script path
+[string]$ScriptPath = Split-Path (Get-Variable MyInvocation -Scope Script).Value.Mycommand.Definition -Parent
+
+# load localized language
+Import-LocalizedData -BindingVariable 'Messages' -FileName 'Messages' -BaseDirectory (Join-Path $ScriptPath 'lang')
+
+'@
 
 # copy all public script contents to module
-Add-Content -Path $ModuleFile -Value '#region PUBLIC '.PadRight(80,'=')
 Get-ChildItem -Path (Join-Path $ScriptPath 'src\public') -Recurse -Filter "*.ps1" -File |
     ForEach-Object {
 
-        Add-Content -Path $ModuleFile -Value "#region $($_.Name) ".PadRight(80,'=')
-        (Get-Content -Path $_.FullName -Raw).Trim("`r`n") | Add-Content -Path $ModuleFile
-        Add-Content -Path $ModuleFile -Value "#endregion $($_.Name) ".PadRight(80,'=')
+        Get-Content -Path $_.FullName -Raw | Add-Content -Path $ModuleFile
         
         # Find all the functions defined no deeper than the first level deep and export it.
         # This looks ugly but allows us to not keep any uneeded variables from poluting the module.
@@ -39,35 +46,22 @@ Get-ChildItem -Path (Join-Path $ScriptPath 'src\public') -Recurse -Filter "*.ps1
                 $ExportModuleMembers += @( $_.Name )
             }
     }
-Add-Content -Path $ModuleFile -Value '#endregion PUBLIC '.PadRight(80,'=')
 
 # copy all private script contents to module
-Add-Content -Path $ModuleFile -Value '#region PRIVATE '.PadRight(80,'=')
 Get-ChildItem -Path (Join-Path $ScriptPath 'src\private') -Recurse -Filter "*.ps1" -File |
     ForEach-Object {
 
-        Add-Content -Path $ModuleFile -Value "#region $($_.Name) ".PadRight(80,'=')
-        (Get-Content -Path $_.FullName -Raw).Trim("`r`n") | Add-Content -Path $ModuleFile
-        Add-Content -Path $ModuleFile -Value "#endregion $($_.Name) ".PadRight(80,'=')
+        Get-Content -Path $_.FullName -Raw | Add-Content -Path $ModuleFile
 
     }
-Add-Content -Path $ModuleFile -Value '#endregion PRIVATE '.PadRight(80,'=')
 
 # copy all private script contents to module
-Add-Content -Path $ModuleFile -Value '#region LIB '.PadRight(80,'=')
 Get-ChildItem -Path (Join-Path $ScriptPath 'lib') -Filter "*.ps1" -File |
     ForEach-Object {
 
-        Add-Content -Path $ModuleFile -Value "#region $($_.Name) ".PadRight(80,'=')
-        (Get-Content -Path $_.FullName -Raw).Trim("`r`n") | Add-Content -Path $ModuleFile
-        Add-Content -Path $ModuleFile -Value "#endregion $($_.Name) ".PadRight(80,'=')
+        Get-Content -Path $_.FullName -Raw | Add-Content -Path $ModuleFile
 
     }
-Add-Content -Path $ModuleFile -Value '#endregion LIB '.PadRight(80,'=')
-
-# copy all lib sub-directories to module
-Get-ChildItem -Path $ScriptPath\lib -Directory |
-    Copy-Item -Destination { "$BuildDirectory\lib\$($_.Name)" } -Recurse
 
 # update the build version
 $ModuleManifestSplat = @{
